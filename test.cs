15db308fb4af676ba6b3539f570342e5d59753fa
JackElias12 <159176795+JackElias12@users.noreply.github.com>
2024-10-08 16:21:33 +0000

    test

---
+++ test.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter name:");
+
+      string userName = Console.ReadLine();
+
+      Console.WriteLine("Hello: " + userName);
+    }
+  }
+}
